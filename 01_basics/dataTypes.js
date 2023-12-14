25fb73e5e55a1a0b7c96a8bf89c70de814bf9af5
hansraj2003 <124131938+hansraj2003@users.noreply.github.com>
2023-12-14 16:20:06 +0000

    DataTypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all JS as newer version
+
+// alert (3+3); // we are using nodeJs not browser as runtime
+
+let name = "Hansraj"
+let age = 20
+let isLoggedIn = false
+let state;
+
+/* DATA TYPES
+numbers => 2 to power of 53
+bigInt => for big numbers in building websites like reddit, websites related to stock market etc.
+string => ""
+boolean => true or false
+null => stand alone value
+undefined => var is given but value is not defined
+symbol => uniqueness
+
+object
+*/
+
+console.log(typeof undefined) // o/p -> undefined
+console.log(typeof null)  // o/p -> object
