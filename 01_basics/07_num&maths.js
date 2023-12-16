78b2add3c14b2283c150fb0e30b026e32bf008c9
hansraj2003 <124131938+hansraj2003@users.noreply.github.com>
2023-12-16 13:41:32 +0000

    07_num&maths

---
+++ 01_basics/07_num&maths.js
@@ -0,0 +1,29 @@
+const score = 300;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+const otherNumber = 25.8459
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 10000000
+
+// console.log(hundreds.toLocaleString(`en-IN`));
+
+
+// ************************************************** Maths **************************************************
+
+// console.log(Math);
+// console.log(Math.abs(-4));            // only -ve values changes to +ve;    Vice-versa not possible
+// console.log(Math.round(4.3));  
+// console.log(Math.ceil(4.2));             // rounds off the value to its greater integral value
+// console.log(Math.floor(4.6));            // rounds off the value to its smaller integral value
+// console.log(Math.min(4, 6, 9, 11)); 
+// console.log(Math.max(4, 6, 9, 11));
+
+console.log(Math.random());
