cb194988592f3dd8b1343d740eef3a22fd79ea29
hansraj2003 <124131938+hansraj2003@users.noreply.github.com>
2023-12-16 07:37:19 +0000

    conversionOperation

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -45,3 +45,9 @@ let negValue = -value
 // console.log (negValue)
 
 // console.log(2**3); // 2 to the power 3
+
+console.log("1"+ 2);
+console.log(1+ "2");
+console.log("1"+ 2 + 2);
+console.log(1+ 2 + "2");
+console.log(1+ "2" + 2);
