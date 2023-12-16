9eb1556e4b6ca3251249611e3f5156a0ce843e06
hansraj2003 <124131938+hansraj2003@users.noreply.github.com>
2023-12-16 10:55:29 +0000

    04_comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,14 @@
+console.log( null > 0);
+console.log( null == 0);
+console.log( null >= 0);
+
+// Comparision operators converts null to number
+// whereas equality check == does not.
+
+console.log( undefined > 0);
+console.log( undefined == 0);
+console.log( undefined >= 0);
+
+// === this is a type of equality check where it checks the datatypes too.
+
+console.log( "2" === 2);
