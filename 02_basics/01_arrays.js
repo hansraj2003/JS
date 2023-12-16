a23ba8b096e54e6dd3dc313344edb69349768836
hansraj2003 <124131938+hansraj2003@users.noreply.github.com>
2023-12-16 18:03:03 +0000

    01_arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,11 @@
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeroes = ["Shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4, 5)
+
+// myArr2.push(6, 7)
+// myArr2.pop()
+
+// myArr2.unshift(10)
+// myArr2.shift()
+console.log(myArr2)
