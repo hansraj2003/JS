eca284a3194b3c74c13e1d7f5dab5710a47b4e54
hansraj2003 <124131938+hansraj2003@users.noreply.github.com>
2023-12-23 10:38:50 +0000

    ??

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -43,3 +43,22 @@ if (userLoggedIn && debitCard && 2 == 2) {
 if (loggedInFromGoogle || loggedInFromEmail) {
     console.log("User logged in");
 }
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 10
+// val1 = null ?? 10 ?? 15
+
+
+// console.log(val1);
+
+//  Terniary Operator
+
+//  condition ? true : false
+
+const iceTeaPrice = 100
+
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
