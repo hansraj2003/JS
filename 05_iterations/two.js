8fa7a8906f54a0b532b14eba0f20159561e61fcf
hansraj2003 <124131938+hansraj2003@users.noreply.github.com>
2023-12-23 12:13:53 +0000

    do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+let index = 0
+while (index <= 10) {
+    // console.log(`Value of i is ${index}`);
+    index = index + 2;
+}
+
+let myArray = ["flash", "batman", "superman"]
+let i = 0;
+while (i < myArray.length) {
+    // console.log(myArray[i]);
+    i++;
+}
+
+let score = 11;
+
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
