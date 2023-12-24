bb5ead4e7a6cb2d6f9e6ecac643e46aca6abf4d4
hansraj2003 <124131938+hansraj2003@users.noreply.github.com>
2023-12-24 14:08:09 +0000

    for in loop

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -1,10 +1,31 @@
 const myObject = {
-    js: 'javascript',
-    cpp: 'c++',
-    rb: 'ruby',
-    swift: 'swift by apple',
+    js: "javascript",
+    cpp: "c++",
+    rb: "ruby",
+    swift: "swift by apple",
 }
 
-for (const key of myObject) {
-    console.log(key);
+for (const key in myObject) {
+    console.log(`${key} is shortcut for ${myObject[key]}`);
 }
+
+const programming = ["js", "cpp", "rb", "swift"];
+
+for (const key in programming) {
+    console.log(programming[key]);
+}
+
+// for (const key of programming) {
+//     console.log(key);
+// }
+
+// const map = new Map()
+// map.set("IN", "India")
+// map.set("IN", "India")
+// map.set("USA", "United States of America")
+// map.set("Fr", "France")
+
+// for (const key in map) {                                             // Map is not iteratble in forin loop.
+//     console.log(key);
+// }
+
