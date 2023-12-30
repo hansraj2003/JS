b2201c74be079a8db257a9e5f65435537d736f6c
hansraj2003 <124131938+hansraj2003@users.noreply.github.com>
2023-12-30 10:59:19 +0000

    01_arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -3,12 +3,12 @@ const myHeroes = ["Shaktiman", "naagraj"]
 
 const myArr2 = new Array(0, 1, 2, 3, 4, 5)
 
-// myArr2.push(6, 7)
-// myArr2.pop()
+myArr2.push(6, 7)
+myArr2.pop()
 
-// myArr2.unshift(10)
-// myArr2.shift()
-// console.log(myArr2)
+myArr2.unshift(10)
+myArr2.shift();
+console.log(myArr2)
 
 //  Slice and Splice
 
@@ -16,6 +16,8 @@ console.log("A ", myArr2);
 
 const myn1 = myArr2.slice(1, 3);
 console.log("B ", myn1);
+console.log(myArr2)
 
 const myn2 = myArr2.splice(1, 3);
 console.log("C ", myn2);
+console.log(myArr2);
