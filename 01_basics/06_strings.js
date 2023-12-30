dbe1a2c979d919af48f26b486df63fec2e6329d5
hansraj2003 <124131938+hansraj2003@users.noreply.github.com>
2023-12-30 06:11:27 +0000

    06_string

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -9,10 +9,10 @@ const gameName = new String (`hansraj-game`)
 
 console.log(gameName.__proto__);
 
-// console.log(gameName.length);
-// console.log(gameName.toUpperCase());
-// console.log(gameName.charAt(2));
-console.log(gameName.indexOf(`a`));
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));                                      // for checking index
+// console.log(gameName.indexOf(`a`));
 console.log(gameName);
 
 const newString = gameName.substring(0,6); // index 6 is not included.
@@ -23,7 +23,7 @@ console.log(anotherString);
 
 // const newStringOne = "            Hansraj         ";
 // console.log(newStringOne);
-// console.log(newStringOne.trim());
+// console.log(newStringOne.trim());                                   // trim removes space
 
 const url = "https://hansraj.com/hansraj%20joshi";
 console.log(url.replace('%20', '-'));
